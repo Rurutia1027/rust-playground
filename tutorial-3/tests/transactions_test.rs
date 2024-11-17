4b6b12feb6e79210c160e64f74894dc28a0f3799
Rurutia1027 <rurutia1027@gmail.com>
2024-11-17 16:14:09 +0800

    update rust format and add more test cases

--- tutorial-3/tests/transactions_test.rs
+++ tutorial-3/tests/transactions_test.rs
@@ -10,8 +10,7 @@ mod tests {
     fn generate_random_output() -> Output {
         let random_uuid = Uuid::new_v4();
         let mut rng = rand::thread_rng();
-        let random_to_addr =
-            random_uuid.to_string();
+        let random_to_addr = random_uuid.to_string();
         let random_value: u16 = rng.gen();
 
         Output::new(
@@ -27,10 +26,7 @@ mod tests {
         let trans: Transaction =
             Transaction::new(inputs, outputs);
 
-        println!(
-            "Transaction Debug Info: {:?}",
-            trans
-        );
+        println!("Transaction Debug Info: {:?}", trans);
 
         let mut bytes: Vec<u8> = vec![];
 
@@ -81,27 +77,19 @@ mod tests {
 
         assert!(bytes.len() > 0);
         // 440
-        println!(
-            "length of outer bytes is {}",
-            bytes.len()
-        );
+        println!("length of outer bytes is {}", bytes.len());
 
         // let's convert the outer's bytes into the hash value
         // and continue convert it into hex format
-        let mut hash_str =
-            hex::encode(crypto_hash::digest(
-                crypto_hash::Algorithm::SHA256,
-                &bytes,
-            ));
-        println!(
-            "outer bytes hash in hex {:?}",
-            hash_str
-        );
+        let mut hash_str = hex::encode(crypto_hash::digest(
+            crypto_hash::Algorithm::SHA256,
+            &bytes,
+        ));
+        println!("outer bytes hash in hex {:?}", hash_str);
 
         // here directly invoke inner function to get hash value,
         // and then continue convert it into hex format
-        let mut trans_hash_str =
-            hex::encode(&trans.hash());
+        let mut trans_hash_str = hex::encode(&trans.hash());
 
         println!(
             "trans bytes hash in hex {:?}, \n Transaction[{:?}]",
@@ -113,12 +101,74 @@ mod tests {
     }
 
     #[test]
-    fn test_transaction_create_with_inputs_outputs(
-    ) {
-    }
+    fn test_transaction_create_with_inputs_outputs() {
+        let mut inputs: Vec<Output> = vec![];
+        let mut outputs: Vec<Output> = vec![];
 
-    #[test]
-    fn test_transaction_create_with_inputs_outpus_hash(
-    ) {
+        // generate 10 input & output items and push those to inputs vector and outputs vector
+        for i in 0..10 {
+            let input = generate_random_output();
+            let output = generate_random_output();
+
+            inputs.push(input);
+            outputs.push(output);
+        }
+
+        let trans: Transaction =
+            Transaction::new(inputs, outputs);
+
+        // continue our logic, create outsider bytes and hash
+        // and compare the outer bytes & hash with the trans function return value
+        let mut bytes: Vec<u8> = vec![];
+
+        // append each items in inputs
+        trans
+            .inputs
+            .iter()
+            .flat_map(|item| item.bytes())
+            .for_each(|item| bytes.push(item));
+
+        // append each items in outputs
+        trans
+            .outputs
+            .iter()
+            .flat_map(|item| item.bytes())
+            .for_each(|item| bytes.push(item));
+
+        assert!(bytes.len() > 0);
+        println!("length of outer bytes is {}", bytes.len());
+        assert_eq!(bytes, trans.bytes());
+
+        let mut hash_str = hex::encode(crypto_hash::digest(
+            crypto_hash::Algorithm::SHA256,
+            &bytes,
+        ));
+
+        let mut trans_hash_str = hex::encode(trans.hash());
+
+        // this should be match
+        assert_eq!(hash_str, trans_hash_str);
+
+        // here we continue validate the total value of each inputs and outputs
+        let input_total_value = trans.input_total_value();
+        let outer_input_total_value: u64 =
+            trans.inputs.iter().map(|item| item.value).sum();
+
+        let output_total_value = trans.output_total_value();
+
+        let outer_output_total_value: u64 =
+            trans.outputs.iter().map(|item| item.value).sum();
+
+        // asser the total value of inner calcuated and outer calculated should be the same
+        assert_eq!(outer_input_total_value, input_total_value);
+        assert_eq!(outer_output_total_value, output_total_value);
+
+        // here we validate the input_hashes and output_hashes
+        let input_hash_set = trans.input_hashes();
+        let output_hash_set = trans.output_hashes();
+        // since elements in inputs many converted to multiple items in hash set
+
+        assert!(input_hash_set.len() >= trans.inputs.len());
+        assert!(output_hash_set.len() >= trans.outputs.len());
     }
 }
