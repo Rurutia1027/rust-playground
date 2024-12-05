e8d20a6fa993c6b1c68bac84f0b8d172cb26a796
Rurutia1027 <rurutia1027@gmail.com>
2024-12-05 10:19:14 +0800

    fix cicd error

--- tutorial-5/src/health.rs
+++ tutorial-5/src/health.rs
@@ -2,9 +2,12 @@ use axum::{
     response::{IntoResponse, Response},
     Json,
 };
+use chrono::{DateTime, Utc};
+use hyper::body::to_bytes;
 use reqwest::StatusCode;
 use serde_json::json;
 
+#[derive(Clone, Debug, PartialEq)]
 pub enum HealthStatus {
     Healthy,
     Unhealthy(Option<String>),
@@ -12,6 +15,7 @@ pub enum HealthStatus {
 
 pub trait HealthCheckable {
     fn health_status(&self) -> HealthStatus;
+    fn get_since_last_update(&self) -> DateTime<Utc>;
 }
 
 impl IntoResponse for HealthStatus {
@@ -26,5 +30,29 @@ impl IntoResponse for HealthStatus {
                 (StatusCode::SERVICE_UNAVAILABLE, Json(body)).into_response()
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_health_status() {
+        let item = HealthStatus::Healthy;
+        let response = item.into_response();
+        assert_eq!(response.status(), StatusCode::OK);
+        println!("status {:?}", response.status());
+        let body_bytes = to_bytes(response.into_body()).await.unwrap();
+        let body_str = String::from_utf8(body_bytes.to_vec()).unwrap();
+        println!("body {:?}", body_str);
+    }
+
+    #[tokio::test]
+    async fn test_unhealth_status() {
+        let item = HealthStatus::Unhealthy(Some("unhealth status".to_string()));
+        let response = item.into_response();
+        assert_eq!(response.status(), StatusCode::SERVICE_UNAVAILABLE);
+        println!("unhealth response: {:?}", response.body());
     }
 }
--- tutorial-5/src/serve/health.rs
+++ tutorial-5/src/serve/health.rs
@@ -1,29 +1,56 @@
-use crate::health;
 use crate::health::{HealthCheckable, HealthStatus};
-use chrono::{DateTime, Duration, Utc};
+use chrono::{DateTime, Duration, TimeDelta, Utc};
+use core::time;
+use enum_iterator::last;
 use std::sync::RwLock;
+use std::thread;
 
 pub struct ServeHealth {
+    health_duration_in_seconds: RwLock<Option<u32>>,
     last_cache_update: RwLock<Option<DateTime<Utc>>>,
     started_on: DateTime<Utc>,
 }
 
-impl ServeHealth {}
+impl ServeHealth {
+    pub fn new(started_on: DateTime<Utc>) -> Self {
+        Self {
+            health_duration_in_seconds: RwLock::new(Some(5)),
+            last_cache_update: RwLock::new(None),
+            started_on,
+        }
+    }
+
+    pub fn set_cache_updated(&self) {
+        *self.last_cache_update.write().unwrap() = Some(Utc::now())
+    }
+
+    pub fn set_health_duration_in_seconds(&self, duration: u32) {
+        assert!(duration >= 0);
+        *self.health_duration_in_seconds.write().unwrap() = Some(duration);
+    }
+}
 
 impl HealthCheckable for ServeHealth {
-    // Cache is healthy if we have seen an update in the last five minutes, or it has been less
-    // than 5 minutes since the server started.
-    fn health_status(&self) -> HealthStatus {
-        let now = Utc::now();
+    fn get_since_last_update(&self) -> DateTime<Utc> {
         let last_update = self
             .last_cache_update
             .read()
             .unwrap()
             .unwrap_or(self.started_on);
+        last_update
+    }
 
-        let time_since_last_update = now - last_update;
+    // Cache is healthy if we have seen an update in the last five minutes, or it has been less
+    // than 5 minutes since the server started.
+    fn health_status(&self) -> HealthStatus {
+        let time_since_last_update = Utc::now() - self.get_since_last_update();
+        let health_duration_in_secs = self
+            .health_duration_in_seconds
+            .read()
+            .unwrap()
+            .unwrap_or(5 * 60) as i64;
 
-        if time_since_last_update < Duration::minutes(5) {
+        if time_since_last_update < Duration::seconds(health_duration_in_secs) {
             HealthStatus::Healthy
         } else {
             HealthStatus::Unhealthy(Some(format!(
@@ -31,6 +58,53 @@ impl HealthCheckable for ServeHealth {
                 time_since_last_update.num_seconds()
             )))
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::time::Duration;
+
+    use chrono::Timelike;
+
+    use super::*;
+    #[tokio::test]
+    async fn test_create_health_status() {
+        let healthy_status = HealthStatus::Healthy;
+        let un_healthy_status = HealthStatus::Unhealthy(Option::None);
+        assert_eq!(healthy_status, HealthStatus::Healthy);
+        assert_eq!(un_healthy_status, HealthStatus::Unhealthy(Option::None));
+    }
+
+    #[tokio::test]
+    async fn test_health_checkable_logic() {
+        let started_on = chrono::Utc::now();
+        let server_health = ServeHealth::new(started_on);
+        let health_status = server_health.health_status();
+        assert_eq!(health_status, HealthStatus::Healthy);
+
+        // here we set the duration threashold to 3 seconds which means
+        // if update duration >= 3 seconds, the health status will become UnHealth
+
+        server_health.set_health_duration_in_seconds(3);
+        server_health.set_cache_updated();
+        // let thread sleep for more than 3 seconds
+        thread::sleep(Duration::from_secs(4));
+        // here we get the health_stauts and it should be in unhealth status
+        let health_status = server_health.health_status();
+        assert_ne!(HealthStatus::Healthy, health_status);
+        let cache_since_last_update = server_health.get_since_last_update();
+        let expected_health_status = HealthStatus::Unhealthy(Some(format!(
+            "cache has not been update in {} seconds",
+            cache_since_last_update.second()
+        )));
+
+        // assert_eq!(expected_health_status, health_status);
+
+        println!(
+            "expected_health_status: {:?}, health_status: {:?}",
+            expected_health_status, health_status
+        );
     }
 }
 
