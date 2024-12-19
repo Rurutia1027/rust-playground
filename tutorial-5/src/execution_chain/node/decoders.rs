bd4aee2ffff0279dd6c8caff20f8fa4fa2292425
Rurutia1027 <rurutia1027@gmail.com>
2024-12-19 15:27:04 +0800

    add test cases and error message to each decoder

--- tutorial-5/src/execution_chain/node/decoders.rs
+++ tutorial-5/src/execution_chain/node/decoders.rs
@@ -33,7 +33,9 @@ where
     // here we conveted the 16 radix string into it's original i32 value
     // string: 0x294724 -> integer: 294724(hex/16 radix) ->  2 * 16^5 + 9 * 16^4 + 4 * 16^3 + 7 * 16^2 + 2 * 16^1 + 4 * 16^0 -> 2_694_356
     // and that's why &s[2..] to skip the first '0x' character
-    Ok(i32::from_str_radix(&s[2..], 16).unwrap())
+    i32::from_str_radix(&s[2..], 16).map_err(|e| {
+        serde::de::Error::custom(format!("Failed to parse hex string: {}", e))
+    })
 }
 
 // function converted serialized string(hex) into unsigned integer 32(bits)
@@ -42,7 +44,17 @@ where
     D: Deserializer<'de>,
 {
     let s: String = Deserialize::deserialize(deserializer)?;
-    Ok(u32::from_str_radix(&s[..2], 16).unwrap())
+
+    if !s.starts_with("0x") {
+        return Err(serde::de::Error::custom(format!(
+            "Expect hex string {} starts with '0x' prefix",
+            s
+        )));
+    }
+
+    u32::from_str_radix(&s[2..], 16).map_err(|e| {
+        serde::de::Error::custom(format!("Failed to parse hex string: {}", e))
+    })
 }
 
 // function converted serialized string(hex) into unsigned integer 64(bits)
@@ -51,7 +63,17 @@ where
     D: Deserializer<'de>,
 {
     let s: String = Deserialize::deserialize(deserializer)?;
-    Ok(u64::from_str_radix(&s[..2], 16).unwrap())
+
+    if !s.starts_with("0x") {
+        return Err(serde::de::Error::custom(format!(
+            "Expect hex string {} starts with '0x' prefix",
+            s
+        )));
+    }
+
+    u64::from_str_radix(&s[2..], 16).map_err(|e| {
+        serde::de::Error::custom(format!("Failed to parse hex string: {}", e))
+    })
 }
 
 // function converted serialized string(hex) into unsigned integer 128(bits)
@@ -60,7 +82,16 @@ where
     D: Deserializer<'de>,
 {
     let s: String = Deserialize::deserialize(deserializer)?;
-    Ok(u128::from_str_radix(&s[..2], 16).unwrap())
+
+    if !s.starts_with("0x") {
+        return Err(serde::de::Error::custom(format!(
+            "Expect hex string {} starts with '0x' prefix",
+            s
+        )));
+    }
+    u128::from_str_radix(&s[2..], 16).map_err(|e| {
+        serde::de::Error::custom(format!("Failed to parse hex string: {}", e))
+    })
 }
 
 // function conver serialized hex timestamp into Utc DateTime
@@ -71,15 +102,23 @@ where
     D: Deserializer<'de>,
 {
     // first convert the serialized string into u32
-    let timestamp_u32 = from_i32_hex_str(deserializer)?;
-    let date_time = Utc.timestamp_opt(timestamp_u32.into(), 0).unwrap();
+    let timestamp_u32 = from_i32_hex_str(deserializer).map_err(|e| {
+        serde::de::Error::custom(format!(
+            "Invalid hex string for timestapm: {}",
+            e
+        ))
+    })?;
 
-    // then converted the u32 into Utc DateTime type
-    Ok(date_time)
+    Utc.timestamp_opt(timestamp_u32.into(), 0)
+        .single()
+        .ok_or_else(|| {
+            serde::de::Error::custom("Failed to convert timestamp to DateTime")
+        })
 }
 
 #[cfg(test)]
 mod tests {
+
     use super::*;
     use serde::{
         de::{Deserialize, Deserializer},
@@ -88,7 +127,7 @@ mod tests {
     use serde_json::{json, Value};
 
     // --- Option<i32>
-    #[derive(Serialize, Deserialize)]
+    #[derive(Serialize, Deserialize, PartialEq, Debug)]
     struct TestStructWith_i32_opt {
         pub id: String,
         #[serde(deserialize_with = "from_i32_opt_hex_str")]
@@ -100,55 +139,143 @@ mod tests {
         let data = json!({"id": "1", "value": "0x294724"});
         let result: TestStructWith_i32_opt =
             serde_json::from_value(data).unwrap();
-        assert_eq!(result.value, Some(2_694_356));
+        assert_eq!(
+            result.value,
+            Some(i32::from_str_radix("294724", 16).unwrap())
+        );
+
+        let obj = TestStructWith_i32_opt {
+            id: "1".to_string(),
+            // 0x20 -> 1 * 16 ^ 0 + 2 * 16 ^ 1
+            //value: Some(1 * 16 ^ 0 + 2 * 16 ^ 1 as i32),
+            value: Some(i32::from_str_radix("20", 16).unwrap()),
+        };
+
+        let json_value = json!({"id": "1", "value": "0x20"});
+        let json_obj =
+            serde_json::from_value::<TestStructWith_i32_opt>(json_value)
+                .unwrap();
+        assert_eq!(json_obj, obj);
     }
 
     // --- i32
 
-    #[derive(Serialize, Deserialize)]
+    #[derive(Serialize, Deserialize, Debug, PartialEq)]
     struct TestStructWith_i32 {
         pub id: String,
         #[serde(deserialize_with = "from_i32_hex_str")]
         pub value: i32,
     }
     #[test]
-    fn from_i32_hex_str_test() {}
+    fn from_i32_hex_str_test() {
+        let data = json!({"id":"2", "value": "0x294724"});
+        let result: TestStructWith_i32 = serde_json::from_value(data).unwrap();
+        assert_eq!(result.value, i32::from_str_radix("294724", 16).unwrap());
 
-    // ---
-    #[derive(Serialize, Deserialize)]
+        let obj = TestStructWith_i32 {
+            id: "2".to_string(),
+            value: i32::from_str_radix("294724", 16).unwrap(),
+        };
+        let json_value = json!({"id":"2", "value": "0x294724"});
+        let json_obj: TestStructWith_i32 =
+            serde_json::from_value::<TestStructWith_i32>(json_value).unwrap();
+        assert_eq!(json_obj, obj);
+    }
+
+    // --- u32
+    #[derive(Serialize, Deserialize, Debug, PartialEq)]
     struct TestStructWith_u32 {
         pub id: String,
         #[serde(deserialize_with = "from_u32_hex_str")]
         pub value: u32,
     }
     #[test]
-    fn from_u32_hex_str_test() {}
+    fn from_u32_hex_str_test() {
+        // u32: min: 0x00000000, max: 0xFFFFFFFF
+
+        let data = json!({"id": "3", "value": "0x00000000"});
+        let result: TestStructWith_u32 = serde_json::from_value(data).unwrap();
+        assert_eq!(result.value, u32::from_str_radix("0", 16).unwrap());
+
+        let json_value = json!({"id":"4", "value": "0xFFFFFFFF"});
+        let json_obj =
+            serde_json::from_value::<TestStructWith_u32>(json_value).unwrap();
+        let obj = TestStructWith_u32 {
+            id: "4".to_string(),
+            value: u32::from_str_radix("FFFFFFFF", 16).unwrap(),
+        };
+        assert_eq!(json_obj, obj);
+    }
 
     //-- u64
-    #[derive(Serialize, Deserialize)]
+    #[derive(Serialize, Deserialize, Debug, PartialEq)]
     struct TestStructWith_u64 {
         pub id: String,
         #[serde(deserialize_with = "from_u64_hex_str")]
         pub value: u64,
     }
-    fn from_u64_hex_str_test() {}
+    fn from_u64_hex_str_test() {
+        // u64, min: 0x0, max: 0xFFFFFFFFFFFFFFFF
+        let data = json!({"id": "5", "value": "0xFFFFFFFFFFFFFFFF"});
+        let result: TestStructWith_u64 = serde_json::from_value(data).unwrap();
+        assert_eq!(
+            result.value,
+            u64::from_str_radix("FFFFFFFFFFFFFFFF", 16).unwrap()
+        );
+
+        let json_value = json!({"id":"6", "value": "0x0"});
+        let json_obj =
+            serde_json::from_value::<TestStructWith_u64>(json_value).unwrap();
+        let obj = TestStructWith_u64 {
+            id: "6".to_string(),
+            value: u64::from_str_radix("0", 16).unwrap(),
+        };
+        assert_eq!(obj, json_obj);
+    }
 
     //-- u128
-    #[derive(Serialize, Deserialize)]
+    #[derive(Serialize, Deserialize, Debug, PartialEq)]
     struct TestStructWith_u128 {
         pub id: String,
         #[serde(deserialize_with = "from_u128_hex_str")]
         pub value: u128,
     }
-    fn from_u128_hex_str_test() {}
+    fn from_u128_hex_str_test() {
+        // u128, min:0x0  max: 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
+        let data =
+            json!({"id": "7", "value": "0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF"});
+        let result: TestStructWith_u128 = serde_json::from_value(data).unwrap();
+        assert_eq!(
+            result.value,
+            u128::from_str_radix("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF", 16)
+                .unwrap()
+        );
+
+        let obj = TestStructWith_u128 {
+            id: "7".to_string(),
+            value: u128::from_str_radix("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF", 16)
+                .unwrap(),
+        };
+
+        assert_eq!(obj, result);
+    }
 
     //-- datetime utc
-    #[derive(Serialize, Deserialize)]
+    #[derive(Serialize, Deserialize, Debug, PartialEq)]
     struct TestStructWith_ts_hex {
         pub id: String,
         #[serde(deserialize_with = "from_unix_timestamp_hex_str")]
         pub value: DateTime<Utc>,
     }
     #[test]
-    fn from_unix_timestamp_hex_str_test() {}
+    fn from_unix_timestamp_hex_str_test() {
+        let data = json!({"id": "8", "value": "0x5f5b0d9d"});
+        let result = serde_json::from_value::<TestStructWith_ts_hex>(data);
+        let date_time = result.unwrap().value;
+
+        let invalid_data = json!({"id": "8", "value": "0xFFFFFFFFF"});
+        let result =
+            serde_json::from_value::<TestStructWith_ts_hex>(invalid_data);
+        assert!(result.is_err());
+    }
 }
